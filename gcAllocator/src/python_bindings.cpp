23d77c121b574c7b2c5d547406ece5c1bedd42a5
Jack Ogaja <12779448+JackOgaja@users.noreply.github.com>
2025-09-11 23:47:41 +0200

    Refactor logging functions in python_bindings.cpp

--- gcAllocator/src/python_bindings.cpp
+++ gcAllocator/src/python_bindings.cpp
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2025 Jack Ogaja
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/chrono.h>
@@ -52,20 +76,11 @@ PYBIND11_MODULE(gc_allocator_core, m) {
         }
     }, "Reset allocation statistics");
     
-    m.def("set_logging_enabled", [](bool enabled) {
-        auto* allocator = GCAllocatorManager::getInstance().getAllocator();
-        if (allocator) {
-            allocator->setLoggingEnabled(enabled);
-        } else {
-            throw std::runtime_error("GCAllocator is not installed");
-        }
-    }, py::arg("enabled"), "Enable or disable logging");
+    m.def("enable_logging", []() {
+        GCAllocatorManager::getInstance().enableLogging();
+    }, "Enable detailed allocation logging");
     
-    m.def("is_logging_enabled", []() {
-        auto* allocator = GCAllocatorManager::getInstance().getAllocator();
-        if (allocator) {
-            return allocator->isLoggingEnabled();
-        }
-        return false;
-    }, "Check if logging is enabled");
+    m.def("disable_logging", []() {
+        GCAllocatorManager::getInstance().disableLogging();
+    }, "Disable detailed allocation logging");
 }
